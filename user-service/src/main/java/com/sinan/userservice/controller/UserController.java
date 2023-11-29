f245ed1de14962090b78ae4465f6f6ffcaff864c
sinaanuzun <sinanuzun43@gmail.com>
2023-11-29 23:33:54 +0300

    update

--- user-service/src/main/java/com/sinan/userservice/controller/UserController.java
+++ user-service/src/main/java/com/sinan/userservice/controller/UserController.java
@@ -6,7 +6,6 @@ import com.sinan.userservice.service.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
